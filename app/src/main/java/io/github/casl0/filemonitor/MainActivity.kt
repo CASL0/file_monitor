1a0670bbfd783df25006ce24249a6e1f280693ea
CASL0 <gin.0046.822@gmail.com>
2023-11-07 23:57:40 +0900

    feat: ファイル監視開始用のUIを仮置き

--- app/src/main/java/io/github/casl0/filemonitor/MainActivity.kt
+++ app/src/main/java/io/github/casl0/filemonitor/MainActivity.kt
@@ -1,20 +1,39 @@
 package io.github.casl0.filemonitor
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import io.github.casl0.filemonitor.service.FileMonitoringService
 import io.github.casl0.filemonitor.ui.theme.FileMonitorTheme
 
 class MainActivity : ComponentActivity() {
+    /** ファイル監視サービス */
+    private var fileMonitoringService: FileMonitoringService? = null
+
+    /** ViewModel */
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        connectService()
         setContent {
             FileMonitorTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +41,61 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val uiState by viewModel.uiState.collectAsState()
+
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = {
+                            if (uiState.monitoringNow) {
+                                fileMonitoringService?.stop()
+                                viewModel.enableMonitoring(false)
+                            } else {
+                                fileMonitoringService?.start(
+                                    applicationContext.filesDir,
+                                    this@MainActivity::onFileChange,
+                                )
+                                viewModel.enableMonitoring(true)
+                            }
+                        }) {
+                            Text(
+                                text = if (uiState.monitoringNow) "STOP MONITORING" else "START MONITORING"
+                            )
+                        }
+
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    /** ファイル変更コールバック */
+    private fun onFileChange(event: Int, path: String?) {
+        Log.i(TAG, "onFileChange, $event, $path")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FileMonitorTheme {
-        Greeting("Android")
+    /** ファイル監視サービスに接続します */
+    private fun connectService() {
+        Intent(this, FileMonitoringService::class.java).also {
+            bindService(
+                it,
+                object : ServiceConnection {
+                    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                        Log.d(TAG, "onServiceConnected, ${name.toString()}, ${service.toString()}")
+                        val binder = service as FileMonitoringService.FileMonitoringBinder
+                        fileMonitoringService = binder.service
+                        viewModel.serviceConnected()
+                    }
+
+                    override fun onServiceDisconnected(name: ComponentName?) {
+                        Log.d(TAG, "onServiceDisconnected, ${name.toString()}")
+                    }
+                },
+                Context.BIND_AUTO_CREATE,
+            )
+        }
     }
 }
+
+private const val TAG = "MainActivity"
---
+++ app/src/main/java/io/github/casl0/filemonitor/MainViewModel.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023 CASL0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.casl0.filemonitor
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+/** MainActivityのUI状態 */
+internal data class MainUiState(
+    /** サービス接続済み */
+    val serviceConnected: Boolean = false,
+    /** ファイル監視中 */
+    val monitoringNow: Boolean = false,
+)
+
+/** MainActivityのビジネスロジックを扱う */
+internal class MainViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState: StateFlow<MainUiState> = _uiState
+
+    /** ファイル監視サービス接続通知 */
+    fun serviceConnected() {
+        _uiState.update { it.copy(serviceConnected = true) }
+    }
+
+    /**
+     * ファイル監視状態変更
+     *
+     * @param enable 監視を有効にした場合はtrue、それ以外はfalse
+     */
+    fun enableMonitoring(enable: Boolean) {
+        _uiState.update { it.copy(monitoringNow = enable) }
+    }
+}
